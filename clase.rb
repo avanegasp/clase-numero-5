2cc01d0e47b2f914b70f3b96a62d67df9570e84f
Angie <arco527@gmail.com>
2018-10-12 20:38:55 -0500

    Ruby class number 5

---
+++ clase.rb
@@ -0,0 +1,53 @@
+#MÉTODOS
+
+
+# def say_hi("cristin")
+#   puts "hola #{"cristin"}, como estás?"
+# end
+#
+# # puts "hola, como estás?"
+# say_hi("cristin")
+#
+# # puts "hola, como estás?"
+# say_hi("miguel")
+#
+# # puts "hola, como estás?"
+# say_hi("carlos")
+
+
+# def suma(1,2)
+#   return 3
+# end
+
+# puts suma(1,2)
+# puts 3
+
+#MÓDULO
+# def num(a,b)
+#   return a % b
+# end
+# puts num(25,3)
+
+#división
+# def divide(num1, num2)
+#   (num1/num2.to_f)
+# end
+#   puts divide(5,2)
+
+
+#MANIPULACIÓN DE ARCHIVOS
+# puts File.read("archivo1.txt")
+# p IO.readlines("archivo1.txt")
+
+# if File.file?("archivo1.txt")
+#   puts "Si existe"
+# else
+#   puts "Ese archivo no existe"
+# end
+
+File.write("archivo2.txt","hola estudiantes")
+
+
+#archivo = archivo1.txt
+#archivoruby = clase5.rb
+#contenido = "hola mundo, como estás?"
---
+++ faker.rb
@@ -0,0 +1,6 @@
+require 'faker'
+
+10.times do
+  puts Faker::Pokemon.name
+  # puts Faker::Superhero.nameend
+end
